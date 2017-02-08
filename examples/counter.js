ec043bbb359add65c33c7e84c3605737a2ac660d
Diego Rodríguez <diegorbaquero@gmail.com>
2017-02-07 23:43:49 -0500

    Actual

--- examples/counter.js
+++ examples/counter.js
@@ -1,4 +1,7 @@
 const V = require('../lib/v')
 const v = new V('counter-example')
-console.log(v.counter++)
+
+console.log(v.counter)
+v.counter++
+console.log(v.counter)
 v.close()
--- test/construct.js
+++ test/construct.js
@@ -5,17 +5,17 @@ const V = require('../lib/v')
 
 test('New V - Empty ID', t => {
   t.plan(1)
-  let v = new V()
+  const v = new V()
   t.pass('Construct worked')
-  v.close() // TODO: Change to destroy
+  v.close() // TODO: Change to destroy, we don't want to keep it in the cloud
 })
 
 test('New V - Const', t => {
   t.plan(2)
-  let v = new V('const-test')
+  const v = new V('const-test')
   t.pass('Construct worked')
   t.equals(v.c, 10)
   v.c = 20
   t.equals(v.c, 10)
-  v.close() // TODO: Change to destroy
+  v.close() // TODO: Change to destroy, we don't want to keep it in the cloud
 })
