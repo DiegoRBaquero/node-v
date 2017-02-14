07344c4dc0a99551fc9248646a4889d1bd3444a6
Diego Rodríguez <diegorbaquero@gmail.com>
2017-02-14 11:46:35 -0500

    Improve example

--- examples/counter.js
+++ examples/counter.js
@@ -1,7 +1,7 @@
-const V = require('v')
+const V = require('../index')
 const v = new V('counter-example')
 
-console.log(v.counter)
+console.log('The value on the cloud was', v.counter)
 v.counter++
-console.log(v.counter)
+console.log('New value is', v.counter)
 v.close()
